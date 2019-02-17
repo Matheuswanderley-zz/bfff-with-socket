1df38173efb2d7a4b1f1803825be0020430d10de
matheus <matheuswanderley1988@gmail.com>
2019-02-16 23:01:12 -0200

    refactoring of routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const httpClient = require('../http-client');
+const service = require('../service');
 const setHeaders = (res) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -12,18 +13,35 @@ router.get("/", (req, res) => {
   res.send({ response: res.data }).status(200);
 });
 
+router.get("/deputados", (req, res)=>{
+  setHeaders(res)
+  console.log('[GET] /deputados')
+
+  service.getDeputados()
+    .then(result => {
+      console.log('[GET] /deputados response 200')
+      res
+        .json({
+          source: 'Deputados API', 
+          ...result, 
+        })
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message })
+    })
+})
+
 
 router.get("/posts/:id", (req, res) =>{
   const id = req.params.id
   httpClient.getPosts(id)
   .then(result =>{
     setHeaders(res)
-    console.log('resultado dos posts', result)
     res.json(result)
   })
   .catch(err =>{
     console.log('err', err)
-    res.json({message: 'erro ao consultar os posts'}).status(500)
+    res.json({ error: err.message}).status(500)
   })
 })
 module.exports = router;
